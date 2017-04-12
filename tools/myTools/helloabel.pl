09b8e294cf6530425d563220d31173473a59e3bc
Sabry Razick <sabryr@gmail.com>
2017-04-12 11:54:47 +0200

    Added more infor to helloabel

--- tools/myTools/helloabel.pl
+++ tools/myTools/helloabel.pl
@@ -10,20 +10,33 @@ open(OUT, ">$ARGV[0]") or die "Can't open $ARGV[0]: $!\n";
 #sleep(100);
 my $time = localtime();
 my $hostname = hostname();
+my $user_email = $ARGV[1];
+if(length($user_email)>=8){
+ $user_email=substr $user_email, 0, 8;  
+}
+
+
+#my $userl = $ENV{'SLURM_JOB_USER'};
 #my $usage = system("resusage > usage");
 
 #foreach my $key (sort keys %ENV ){
 #    print OUT "$key: $ENV{$key}\n";
 #}
 
-
-print  OUT "Abel is up and running. \nIt is $time on $hostname.\n\n";
-print  OUT "Abel resources:\n";
+print  OUT "Abel is up and running.\n";
+print  OUT "It is $time on $hostname\n\n";
+print  OUT "Abel resources:";
 close( OUT );
+system("echo  >> $ARGV[0]");
 system("resusage >> $ARGV[0]");
 system("echo >> $ARGV[0]");
-system("echo 'Number of Lifeportal jobs on Cluster' >> $ARGV[0]");
+system("echo 'Your Lifeportal jobs on Cluster' >> $ARGV[0]");
+system("squeue | grep $user_email  >> $ARGV[0]");
+system("echo  >> $ARGV[0]");
+system("echo 'Total number of Lifeportal jobs on Cluster' >> $ARGV[0]");
 system("squeue | grep galaxy | wc -l >> $ARGV[0]");
+system("echo  >> $ARGV[0]");
+#system("echo $user_email >> $ARGV[0]");
 
 
 
